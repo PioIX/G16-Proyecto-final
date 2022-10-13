7740b93efe082d54be3a4594166570e41428fb7e
Ldoom Nicolás <103506112+lautaroMiguez@users.noreply.github.com>
2022-10-13 15:22:07 +0000

    cambios agregados

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
 
 app = Flask(__name__)
 
@@ -9,13 +11,33 @@ def index():
 
 @app.route('/ingresado', methods = ['GET', 'POST'])
 def ingreso():
-  pass
+  if (request.method == "POST"):
+    if(request.form["nombre"] !=""):
+      conn = sqlite3.connect('5EnLinea.db')
+      usuario = request.form['nombre']
+      password = request.form['contra']
+      q = f"""SELECT * FROM Usuarios WHERE Nombre = '{usuario}' and Contraseña = '{password}';"""
+      resu = conn.execute(q)
+
+
+      if resu.fetchone():
+        conn.commit()
+        conn.close() 
+        return redirect("/inicio")
+      else: 
+        conn.commit()
+        conn.close() 
+        return render_template('inicio.html')
+  else:
+    return render_template('inicio.html')
+    
+@app.route('/inicio')
+def menu():
+  return render_template('menu.html')
 
 @app.route('/registro', methods = ['GET', 'POST'])
 def registro():
   pass
-
-
 
 
 app.run(host='0.0.0.0', port=81)
