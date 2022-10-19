30f90d98f9fca35682919c82550a7cb26084be79
Ldoom Nicolás <103506112+lautaroMiguez@users.noreply.github.com>
2022-10-19 15:20:42 +0000

    actualizando fork

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 
 app = Flask(__name__)
+app.secret_key = "jajaxd"
 
 
 @app.route('/')
@@ -14,9 +15,9 @@ def ingreso():
   if (request.method == "POST"):
     if(request.form["nombre"] !=""):
       conn = sqlite3.connect('5EnLinea.db')
-      usuario = request.form['nombre']
-      password = request.form['contra']
-      q = f"""SELECT * FROM Usuarios WHERE Nombre = '{usuario}' and Contraseña = '{password}';"""
+      session['usuario'] = request.form['nombre']
+      session['password'] = request.form['contra']
+      q = f"""SELECT * FROM Usuarios WHERE Nombre = '{session['usuario']}' and Contraseña = '{session['password']}';"""
       resu = conn.execute(q)
 
 
@@ -27,17 +28,55 @@ def ingreso():
       else: 
         conn.commit()
         conn.close() 
-        return render_template('inicio.html')
+        return render_template('inicio.html', error = True)
   else:
     return render_template('inicio.html')
-    
-@app.route('/inicio')
-def menu():
-  return render_template('menu.html')
 
 @app.route('/registro', methods = ['GET', 'POST'])
 def registro():
-  pass
+  if (request.method == "POST"):
+    if (request.form["nombre"] != "") and (request.form["contra"] == request.form["contra2"]):
+      conn = sqlite3.connect('5EnLinea.db')
+      nombre = request.form['nombre']
+      contraseña = request.form['contra']
+      q = f"""SELECT * FROM Usuarios WHERE Nombre = '{nombre}' and Contraseña = '{contraseña}';"""
+      s = f"""INSERT INTO Usuarios(nombre, contraseña) VALUES ('{nombre}', '{contraseña}');"""
+      resu = conn.execute(q)
 
+      if resu.fetchone():
+        conn.commit()
+        conn.close()
+        return render_template("Registro.html", error1 = True)
+      else:
+        conn.execute(s)
+        conn.commit()
+        conn.close()
+        return redirect("/")    
+    else:
+      return render_template("Registro.html", error2 = True)
+  else:
+    return render_template("Registro.html")
+  
+    
+@app.route('/inicio', methods=['GET'])
+def menu():
+  return render_template('menu.html', nombre = session['usuario'])
 
+@app.route('/seleccionarSala', methods=['GET'])
+def salaB():
+  return render_template('selectorSalas.html')
+
+@app.route('/crearSala', methods=['GET'])
+def salaC():
+  return render_template('creadorSalas.html')
+
+@app.route('/editarPerfil', methods=['GET'])
+def editPerfil():
+  return render_template('editPerfil.html')
+
+@app.route('/rankings', methods=['GET'])
+def rankings():
+  return render_template('rankings.html')
+
+  
 app.run(host='0.0.0.0', port=81)
---
+++ static/juego.js
@@ -0,0 +1,17 @@
+function vaciarA() {
+  for (let i = 1; i =< 9; i++){
+      A[i]= 0
+  }}
+
+function colocarFicha(valorDeJugador ,posicion) {
+  if (posicion.valor == 0) {
+    posicion.valor = valorDeJugador }
+}
+
+function girarIzquierda(cuadrante) {
+  
+}
+
+function girarDerecha(cuadrante) {
+  
+}
