8e55125dcf36b82fce8a5fc6726c77c2ac09b20f
Santiago Martin Martin <100937259+Sbalanzat914@users.noreply.github.com>
2022-12-14 12:17:57 +0000

    Version final finalisima definitiva de las versiones.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def registro():
       nombre = request.form['nombre']
       contraseña = request.form['contra']
       q = f"""SELECT * FROM Usuarios WHERE Nombre = '{nombre}';"""
-      s = f"""INSERT INTO Usuarios(nombre, contraseña, puntaje) VALUES ('{nombre}', '{contraseña}', 0);"""
+      s = f"""INSERT INTO Usuarios(Nombre, Contraseña, Puntaje) VALUES ('{nombre}', '{contraseña}', 0);"""
       resu = conn.execute(q)
 
       if resu.fetchone():
@@ -63,11 +63,19 @@ def registro():
     
 @app.route('/inicio')
 def menu():
-  return render_template('menu.html', nombre = session['usuario'])
+  conn = sqlite3.connect('Usuarios.db')
+  a = f"""SELECT ID FROM Usuarios WHERE Nombre = '{session['usuario']}'"""
+  elID = conn.execute(a)
+  elIDBien = elID.fetchone()
+  session['ID'] = elIDBien[0]
+  if (session['usuario'] == "admin"):
+    return redirect('/inicioAdmin')
+  else:
+    return render_template('menu.html', nombre = session['usuario'])
 
 @app.route('/inicioAdmin')
 def menuAdmin():
-  return render_template('menuAdmin.html')
+  return render_template('menuAdmin.html', nombre = session['usuario'])
 
 @app.route('/juego')
 def jueguito():
@@ -99,39 +107,37 @@ def editPerfilConfirm():
           if (request.form['nombre'] == session['usuario']):
             return render_template("editPerfil.html", error1 = True)
           elif asu.fetchone():
-            return render_template("editPerfil.html", error4 = True)
+            return render_template("editPerfil.html", error3 = True)
           else:
-            a = f"""UPDATE Usuarios SET Nombre = '{request.form['nombre']}' where Nombre = '{session['usuario']}';"""
-            session['usuario'] = request.form['nombre']
+            b=f"""DELETE FROM Usuarios WHERE Nombre ='{session['usuario']}'"""
+            a = f"""INSERT INTO Usuarios(Nombre, Contraseña, Puntaje, ID) VALUES ('{request.form['nombre']}', '{session['password']}', 0, '{session['ID']}')"""
+            conn.execute(b)
             conn.execute(a)
             conn.commit()
             conn.close()
             return redirect('/')
-        elif (request.form['nombre'] == "") and (request.form['contra'] != " ") and (request.form['contra'] == request.form['contra2']):
-            if (session['password'] == request.form['contra']):
-              return render_template("editPerfil.html", error2 = True)
-            elif (request.form['contra'] != request.form['contra2']):
-              return render_template("editPerfil.html", error3 = True)
-            else:
-              a = f"""UPDATE Usuarios SET Contraseña = '{request.form['contra']}' where Contraseña = '{session['password']}';"""
-              session['password'] = request.form['contra']
-              conn.execute(a)
-              conn.commit()
-              conn.close()
-              return redirect('/')
-        elif (request.form['nombre'] != "") and (request.form['contra'] != "") and (request.form['contra'] == request.form['contra2']):
+        elif(request.form['nombre'] == "") and (request.form['contra'] != ""):
+          if (request.form['contra'] == session['password']):
+            return render_template("editPerfil.html", error2 = True)
+          else:
+            b=f"""DELETE FROM Usuarios WHERE Nombre ='{session['usuario']}'"""
+            a=f"""INSERT INTO Usuarios(Nombre, Contraseña, Puntaje, ID) VALUES ('{session['usuario']}', '{request.form['contra']}', 0, '{session['ID']}')"""
+            conn.execute(b)
+            conn.execute(a)
+            conn.commit()
+            conn.close()
+            return redirect('/')
+        elif (request.form['nombre'] != "") and (request.form['contra'] != ""):
           if (request.form['nombre'] == session['usuario']):
             return render_template("editPerfil.html", error1 = True)
-          elif (session['password'] == request.form['contra']):
-              return render_template("editPerfil.html", error2 = True)
-          elif (request.form['contra'] != request.form['contra2']):
-            return render_template("editPerfil.html", error3 = True)
           elif asu.fetchone():
-            return render_template("editPerfil.html", error4 = True)
+            return render_template("editPerfil.html", error3 = True)
+          elif (request.form['contra'] == session['password']):
+            return render_template("editPerfil.html", error2 = True)
           else:
-            a = f"""UPDATE Usuarios SET Nombre = '{request.form['nombre']}' AND Contraseña = '{request.form['contra']}' WHERE Nombre = '{session['usuario']}' AND Contraseña = '{session['password']}';"""
-            session['usuario'] = request.form['nombre']
-            session['password'] = request.form['contra']
+            b=f"""DELETE FROM Usuarios WHERE Nombre ='{session['usuario']}'"""
+            a= f"""INSERT INTO Usuarios(Nombre, Contraseña, Puntaje, ID) VALUES ('{request.form['nombre']}', '{request.form['contra']}', 0, '{session['ID']}')"""
+            conn.execute(b)
             conn.execute(a)
             conn.commit()
             conn.close()
@@ -160,12 +166,13 @@ def rankings():
       idPasado = id.fetchone()
       nombrePasado = nombre.fetchone()
       puntajePasado = puntaje.fetchone()
-      nombres.append(nombrePasado[0])
-      ids.append(idPasado[0])
-      puntajes.append(puntajePasado[0])
-      ip += 1
-    conn.commit()
-    conn.close()
+
+      if nombrePasado != None and idPasado != None and puntajePasado != None:
+        nombres.append(nombrePasado[0])
+        ids.append(idPasado[0])
+        puntajes.append(puntajePasado[0])
+        ip += 1
+      print(nombres, ids, puntajes)
     return render_template("rankings.html", nombres1 = nombres, ids1 = ids, puntajes1 = puntajes)
   
 
@@ -181,16 +188,16 @@ def destruccion():
 @app.route('/noPoints', methods=['GET'])
 def borrarPuntos():
   conn = sqlite3.connect('Usuarios.db')
-  q = """UPDATE Ranking SET Puntaje = '0';"""
+  q = """UPDATE Usuarios SET Puntaje = '0';"""
   conn.execute(q)
   conn.commit()
   conn.close()
   return redirect('/inicioAdmin')
 
-@app.route('/cerrarSesion', methods=['GET'])
+@app.route('/cerrarSesion', methods = ['GET'])
 def cerrado():
-  session['usuario'] = " "
-  session['password'] = " "
+  session['usuario'] = ""
+  session['password'] = ""
   return redirect('/')
   
 app.run(host='0.0.0.0', port=81)
